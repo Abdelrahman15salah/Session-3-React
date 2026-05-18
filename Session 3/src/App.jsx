5b3bef865d2735125adda4fbd6cc8947ca4a5d52
Abdelrahman Salah <abdelrahman15salah@gmail.com>
2026-05-18 10:59:55 +0300

    added not found component

--- Session 3/src/App.jsx
+++ Session 3/src/App.jsx
@@ -14,6 +14,7 @@ import TV from "./Components/Electrons/Contents/TV";
 import Car from "./Components/Electrons/Contents/Car";
 import Phone from "./Components/Electrons/Contents/Phone";
 import Childlayout from "./Components/Electrons/Contents/Childlayout";
+import NotFound from "./Components/NotFound";
 
 let x = createBrowserRouter([
   {
@@ -31,7 +32,7 @@ let x = createBrowserRouter([
         element: <Electrons />,
       },
       {
-        path: "Electrons/",
+        path: "Electrons",
         element: <Childlayout />,
         children: [
           { path: "TV", element: <TV /> },
@@ -39,6 +40,7 @@ let x = createBrowserRouter([
           { path: "Phone", element: <Phone /> },
         ],
       },
+      { path: "*", element: <NotFound /> },
     ],
   },
 ]);
